71be5fbbb418670d5e40472f78f6313d89d286ae
hoyeon <fnfn0901@sookmyung.ac.kr>
2025-03-18 22:13:17 +0900

    feat: 뒤로가기 제스처 구현

--- Pricedive/Pricedive/Presentation/EventDetail/ViewController/EventDetailViewController.swift
+++ Pricedive/Pricedive/Presentation/EventDetail/ViewController/EventDetailViewController.swift
@@ -56,21 +56,24 @@ class EventDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         detailView.viewModel = viewModel
         setupBindings()
         setupActions()
 
         viewModel.fetchVideoDetail(videoId: videoId)
 
+        // 기존 코드
         DispatchQueue.main.async {
             self.detailView.updateHeartButton(isLiked: self.viewModel.isLiked)
-
             if let initialPreviewImg = self.viewModel.previewImg, let url = URL(string: initialPreviewImg) {
                 self.detailView.imageView.kf.setImage(with: url)
             }
         }
-        
+
+        // 👇 이 부분 추가 (스와이프 살리기)
         navigationController?.interactivePopGestureRecognizer?.delegate = self
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
     
     private func saveViewedProduct() {
